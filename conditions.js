182487dd7aa647a8a18eb398b46692129bc03c48
Adrien Rahier <adrien@circularise.com>
2021-01-13 15:47:24 +0100

    Add new code structure I didn’t know before

--- conditions.js
+++ conditions.js
@@ -39,3 +39,16 @@
   })();
 }
 /* ********************************************* */
+
+// If, else if, else if ternary:
+
+const conditions = { c1: false, c2: false, c3: true };
+const { c1, c2, c3 } = conditions;
+
+```js
+const c1 = false; const c2 = false; const c3 = false; 
+c1 ? "condition 1 is true"
+  : c2 ? "condition 2 is true"
+  : c3 ? "condition 3 is true"
+  : "None of these conditions are true";
+```;
--- loops.js
+++ loops.js
@@ -3,6 +3,26 @@
 
 const myObj = { hip: "hop", ta: "da" };
 
+// Preferred way of looping into objects!
+// Note: Advantage of .entries() compared to .keys():
+// To output the values I don't need to do `myObj[key]` anymore (pb: if the name of your object changes, I also need to change the loop).
+console.log(
+  "index: ",
+  Object.entries(myObj).map(([key, value]) => {
+    return value; // or key for keys
+  })
+);
+
+// JSX way
+Object.entries(myObj).map(([key, value]) => (
+  <p key={key}>
+    {key}: {value}
+  </p>
+));
+
+/////////////////////////
+//////// Legacy: ////////
+/////////////////////////
 console.log(
   "index: ",
   Object.keys(myObj).map((key, i) => {
